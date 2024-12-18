06a200dfc7828263a43956aa8a02a97d92fca11d
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-12-18 12:11:38 +0800

    Initial Commit

---
+++ CountingAnimals/CountingAnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountingAnimalsApp.swift
+//  CountingAnimals
+//
+//  Created by Woodrow Martyr on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CountingAnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

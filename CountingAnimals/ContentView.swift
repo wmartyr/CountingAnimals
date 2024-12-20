8e9351fbabe64d967d2d44f5b07ae7b167e5ca84
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-12-20 08:51:50 +0800

    Format icons

--- CountingAnimals/ContentView.swift
+++ CountingAnimals/ContentView.swift
@@ -10,35 +10,49 @@ import SwiftUI
 struct ContentView: View {
     @State private var animals = ["bear", "buffalo", "chick", "chicken", "cow", "crocodile", "dog", "duck", "elephant", "frog", "giraffe", "goat", "gorilla", "hippo", "horse", "monkey", "moose", "narwhal", "owl", "panda", "parrot", "penguin", "pig", "rabbit", "rhino", "sloth", "snake", "walrus", "whale", "zebra"]
     @State private var numberOfAnimals = 2
-    @State private var gridSize = 4
+    @State private var gridSize = 6
     @State private var chosenAnimals = [""]
-    @State private var finalAnimalList = [""]
+    @State private var finalAnimalList: [Animal] = [Animal(animal: "")]
+    private let adaptiveColumn = [GridItem(.adaptive(minimum: 110))]
     
+    struct Animal {
+        let id = UUID()
+        let animal: String
+    }
+
     var body: some View {
-        ForEach(finalAnimalList, id: \.self) { animal in
-            Text("\(animal)")
-                .font(.largeTitle)
-            Image(animal)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-        }
-        .padding()
-        .onAppear {
-            initialSetup()
+        ScrollView {
+            LazyVGrid(columns: adaptiveColumn, spacing: 10) {
+                ForEach(finalAnimalList, id: \.id) { animal in
+                    VStack {
+                        Image(animal.animal)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                        Text("\(animal.animal)")
+                            .textCase(.uppercase)
+                    }
+                }
+                .padding(10)
+                .onAppear {
+                    initialSetup()
+                }
+            }
         }
     }
     
-    func animalDisplayGridList(_ gridSize: Int) -> [String] {
-        var finalList: [String] = []
+    func animalDisplayGridList(_ gridSize: Int) -> [Animal] {
+        var finalList: [Animal] = []
         for _ in 0..<gridSize {
-            finalList.append(chosenAnimals.randomElement() ?? "bear")
+            finalList.append(Animal(animal: chosenAnimals.randomElement() ?? "bear"))
         }
         return finalList
     }
     
     func initialSetup() {
+        print("Run initialSetup")
         chosenAnimals = Array(animals.shuffled().prefix(numberOfAnimals))
         finalAnimalList = animalDisplayGridList(gridSize)
+        print("finalAnimalList: \(finalAnimalList)")
     }
 }
 
